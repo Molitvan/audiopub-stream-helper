2fe26af2047b3fd5e91f76aa56e89ff28bfaee37
Molitvan <82037155+Molitvan@users.noreply.github.com>
2026-06-21 19:50:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Audiopub Stream Helper
+
+A small utility to enhance the experience of streaming on [Audiopub](https://audiopub.site).
+
+## Features
+
+- Chat narration
+- Sound effects for when listeners leave and join, when the stream ends and when errors happen
+
+Yeah... when I said it's small, I meant it lol.
+
+## Setup
+
+1. [Install UV](https://github.com/astral-sh/uv)
+2. Install Git (if not already installed)
+3. Clone this repository by opening your terminal and running "git clone https://github.com/Molitvan/audiopub-stream-helper"
+4. Open your terminal in the cloned repository folder and run "uv sync"
+
+## Updates
+
+To install updates, run "git pull" from your terminal in the cloned repository folder periodically.
+
+## Usage
+
+Simply open the terminal in the folder where you set everything up and run "uv run main.py"
+
+## Customizing the sounds
+
+You can customize the default sounds by replacing files in the sounds folder. To disable a sound, delete it from the folder or rename it.
+
+## Maintenance
+
+I made this tool in about an hour (including the sounds) and I plan on maintaining it only for how much my needs require. I made it for myself first, but I'm posting it here in case someone else finds it interesting. That said, feel free to open pull requests or fork this if you want something added.
---
+++ main.py
@@ -0,0 +1,81 @@
+import requests
+import prism
+import json
+import sseclient
+import time
+import playsound3
+import sys
+
+connect_sound = "sounds/connect.wav"
+disconnect_sound = "sounds/disconnect.wav"
+listener_join_sound = "sounds/listener_join.wav"
+listener_leave_sound = "sounds/listener_leave.wav"
+error_sound = "sounds/error.wav"
+
+def play(sound: str):
+    try:
+        playsound3.playsound(sound)
+    except:
+        pass
+
+def main(url: str, useSapi: str):
+    if useSapi != "":
+        speech = prism.Context().create(prism.BackendId.SAPI)
+        print("Useing SAPI")
+    else:
+        speech = prism.Context().create_best()
+        print("Using best backend")
+
+    listeners: int = 0
+
+    while True:
+        try:
+            print("Connecting...")
+            play(connect_sound)
+
+            response = requests.get(f"{url}/events", stream=True, timeout=None)
+            response.raise_for_status()
+
+            client = sseclient.SSEClient(response)
+
+            for event in client.events():
+                data = json.loads(event.data) if event.data else {}
+
+                if event.event == "chat":
+                    name = data["user"]["name"]
+                    content = data["content"]
+                    print(f"{name}: {content}")
+                    speech.output(f"{name} said {content}")
+                elif event.event == "listeners":
+                    active = data["activeListeners"]
+
+                    if active > listeners:
+                        play(listener_join_sound)
+                        print(f"Listener joined: {listeners} listeners")
+                    elif active < listeners:
+                        play(listener_leave_sound)
+                        print(f"Listener left: {listeners} listeners")
+
+                    listeners = active
+                elif event.event == "finish":
+                    message = "Stream finished. Exiting..."
+                    speech.output(message)
+                    print(message)
+                    play(disconnect_sound)
+                    return
+        except KeyboardInterrupt:
+            print("Exiting...")
+            play(disconnect_sound)
+            sys.exit(0)
+        except Exception as error:
+            print(f"Disconnected: {error}")
+            print("Trying to reconnect in 5 seconds...")
+            play(error_sound)
+            time.sleep(5)
+
+if __name__ == "__main__":
+    print("Welcome to Audiopub Stream Helper")
+    url = input("Enter the stream URL: ").rstrip("/")
+    useSapi = input("Would you like to force use SAPI for output? Press enter for no, type anything for yes: ")
+    main(url, useSapi)
+    input("Press ENTER to exit")

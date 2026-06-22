fc69e72a229f54dc019d30059cac78bc39ec6de1
Molitvan <82037155+Molitvan@users.noreply.github.com>
2026-06-22 22:30:13 +0200

    Move import statement to the top of the file in random.py

--- commands/random.py
+++ commands/random.py
@@ -1,7 +1,7 @@
 import chat_client
+import random
 
 name = "random"
-import random
 
 def run(data, chat: chat_client.chat_client):
     chat.send_message(f"Your random number is: {random.randint(0, 100)}")
